986e366cba1bf4be6e3b9557302b4d04079377d0
madsjoyce <madeleinevelyn@outlook.com>
2023-11-22 16:10:29 +0000

    'updates functionality of milestone_2.py'

--- milestone_2.py
+++ milestone_2.py
@@ -1,21 +1,31 @@
-import random #imports random function
+import random
+
+def choose_random_word(word_list):
+    """Choose a random word from the given list."""
+    return random.choice(word_list)
+
+def get_player_guess():
+    """Ask the user to input a single letter."""
+    return input('Please enter a single letter:')
+
+def is_valid_guess(guess):
+    """Check if the input is a valid single letter."""
+    return len(guess) == 1 and guess.isalpha()
 
 # My list of fruits:
 word_list = ['apple', 'banana', 'cherry', 'peach', 'mango']
 print(word_list)
 
-#Create a random word from the list
-word = random.choice(word_list)
-print(word)
+# Create a random word from the list
+random_word = choose_random_word(word_list)
+print(random_word)
 
 # Ask user to input letter
-guess = input('Please enter a single letter:')
+player_guess = get_player_guess()
 
-#check input is valid
-if len(guess) == 1 and guess.isalpha():
-  print('Good Guess!')
-
+# Check if input is valid
+if is_valid_guess(player_guess):
+    print('Good Guess!')
 else:
- print("Oops! That is not a valid input.")
-
+    print("Oops! That is not a valid input.")
  
