eb8192eeeb5de3954ff9f89b4b102fdbac35b906
madsjoyce <madeleinevelyn@outlook.com>
2023-11-23 15:55:30 +0000

    'adds new milestone4 and makes old milestone4edit'

--- milestone_4edit.py
+++ milestone_4edit.py
@@ -4,40 +4,26 @@ class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
-        self.pick_new_word()
-
-    def pick_new_word(self):
-        self.word = random.choice(self.word_list)
+        self.word = random.choice(word_list)
         self.word_guessed = ['_'] * len(self.word)
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
-
-    def display_word_guessed(self):
-        print("Word guessed so far:", ' '.join(self.word_guessed))
-
-    def update_word_guessed(self, guess):
-        for i, letter in enumerate(self.word):
-            if letter == guess:
-                self.word_guessed[i] = guess
-
-        self.num_letters -= 1
-
-    def handle_correct_guess(self, guess):
-        print(f'Good guess! {guess} is in the word.')
-        self.update_word_guessed(guess)
-
-    def handle_incorrect_guess(self, guess):
-        self.num_lives -= 1
-        print(f"Sorry, {guess} is not in the word.")
-        print(f"You have {self.num_lives} lives left.")
 
     def check_guess(self, guess):
         guess = guess.lower()
 
         if guess in self.word:
-            self.handle_correct_guess(guess)
+            print(f'Good guess! {guess} is in the word.')
+
+            for i, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[i] = guess
+
+            self.num_letters -= 1
         else:
-            self.handle_incorrect_guess(guess)
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
         while True:
@@ -51,5 +37,5 @@ class Hangman:
             else:
                 self.list_of_guesses.append(guess)
                 self.check_guess(guess)
-                self.display_word_guessed()
+                print("Word guessed so far:", ' '.join(self.word_guessed))
                 break
